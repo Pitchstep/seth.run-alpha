83eb6c63b84d8d9fc49e3ad73b5818e0b9fc0483
Pitchstep <109386708+Pitchstep@users.noreply.github.com>
2024-04-29 19:50:37 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-const form = document.getElementById('passwordForm');
-const passwordInput = document.getElementById('password');
-const errorMessage = document.getElementById('errorMessage');
-const password = 'yourpassword'; // Set your password here
-
-form.addEventListener('submit', function(event) {
-    event.preventDefault();
-    if (passwordInput.value === password) {
-        window.location.href = 'welcome'; // Redirect to welcome page
-    } else {
-        errorMessage.textContent = 'Incorrect password. Please try again.';
-    }
-});
+// its a secret---
